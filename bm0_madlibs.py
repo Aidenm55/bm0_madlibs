b9fe95c47360946bc74b4e4796d88259109a44a0
Aidenm55 <Aidenm3262@nycstudents.net>
2025-12-02 11:19:36 -0500

    Create bm0_madlibs.py

---
+++ bm0_madlibs.py
@@ -0,0 +1,11 @@
+noun = input("Enter random noun:")
+verb = input("Enter random verb")
+adjective = input("Enter random adjective")
+
+print (f"I am a ninja I have fire power and {adjective} ")
+print (f"I am a hot head and {noun} I did my daliy check up on the city")
+print (f"I fought a powerful guy that was very strong and {verb}" )
+print (f"I won the fight  and {noun} with some cut on my ninja suit") 
+print ("but he was my friend and took home and we ate chipotle")
+
+print("HELLO")
